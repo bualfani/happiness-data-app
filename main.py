3f0238cd6a3ba8d2649d3598e0921c9f8a15343f
bahati <bulozealfani@gmail.com>
2023-03-04 14:50:50 -0600

    plotting the graph

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import plotly.express as px
 import pandas as pd
 
 st.title("Happiness Data Graph")
-x = st.selectbox("Select the data for the X-axis", ("GDP", "Happiness", "Generosity"))
-y = st.selectbox("Select The Data from the Y-axis", ("GDP", "Happiness", "Generosity"))
-st.subheader(f"{x} and {y}")
+x_axis = st.selectbox("Select the data for the X-axis", ("GDP", "Happiness", "Generosity"))
+y_axis = st.selectbox("Select The Data from the Y-axis", ("GDP", "Happiness", "Generosity"))
 
 # loading the dataframe
 df = pd.read_csv("happy.csv")
 
-match x:
+# x axis
+match x_axis:
     case "Happiness":
         x_array = df["happiness"]
     case "GDP":
@@ -18,7 +18,8 @@ match x:
     case "Generosity":
         x_array = df["generosity"]
 
-match y:
+# y axis
+match y_axis:
     case "Happiness":
         y_array = df["happiness"]
     case "GDP":
@@ -26,4 +27,8 @@ match y:
     case "Generosity":
         y_array = df["generosity"]
 
-st.plotly_chart()
+st.subheader(f"{x_axis} and {y_axis}")
+
+# plotting the webpage
+figure = px.scatter(x=x_array, y=y_array, labels={'x': x_axis, 'y': y_axis})
+st.plotly_chart(figure)
